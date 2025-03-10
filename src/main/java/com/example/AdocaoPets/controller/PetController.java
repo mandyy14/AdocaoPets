034f72537064298f53ab76e5bb7977943e5dd07b
Amanda Mesquita <amandamesquita73638@gmail.com>
2025-03-09 23:10:07 -0300

    first commit

---
+++ src/main/java/com/example/AdocaoPets/controller/PetController.java
@@ -0,0 +1,5 @@
+package com.example.AdocaoPets.controller;
+
+public class PetController {
+    
+}
---
+++ src/test/java/com/example/AdocaoPets/AdocaoPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AdocaoPets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdocaoPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
