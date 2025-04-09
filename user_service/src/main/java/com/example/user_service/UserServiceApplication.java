ffe13e0c1ec91a53d5fbc795c1d07a1e1c83abd6
JouTiago <162067688+JouTiago@users.noreply.github.com>
2025-04-08 22:46:42 -0300

    swagger

--- user_service/src/main/java/com/example/user_service/UserServiceApplication.java
+++ user_service/src/main/java/com/example/user_service/UserServiceApplication.java
@@ -3,7 +3,11 @@ package com.example.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "User_Service", description = "API para cadastro e login de usuário", version = "v1"))
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
